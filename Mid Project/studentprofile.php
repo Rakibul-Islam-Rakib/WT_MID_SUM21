99613799cfa9c1451eb964414c910d31e29bd66b
Rakibul Islam Rakib <60995134+Rakibul-Islam-Rakib@users.noreply.github.com>
2021-06-22 16:08:27 +0600

    Created

---
+++ Mid Project/studentprofile.php
@@ -0,0 +1,72 @@
+<?php 
+?> 
+<html>
+	<head></head>
+	<body>
+		<form>
+		<fieldset>
+        <center>
+			<table>
+			<tr>
+			<td> <center> <b> Student Profile </center> </td>
+			</tr>
+				<tr>
+				
+					<td>Student Name</td>
+					<td><input type="text" placeholder=" Your Name"> </td>
+				</tr>
+				<tr>
+				
+					<td>Father's Name</td>
+					<td><input type="text" placeholder=" Father's Name"> </td>
+				</tr>
+				<tr>
+				
+					<td>Mother's Name</td>
+					<td><input type="text" placeholder=" Mother's Name"> </td>
+				</tr>
+				<tr>
+					<td>Present Address</td>
+					<td>: <input type="text"  placeholder="Street Address"> <input type="text"  placeholder="City">- <input type="text"  placeholder="State"> <br> <input type="text"  placeholder="Postal/Zip Code">  </td>
+				
+					
+				</tr>
+				<tr>
+					<td>Permanent Address</td>
+					<td>: <input type="text"  placeholder="Street Address"> <input type="text"  placeholder="City">- <input type="text"  placeholder="State"> <br> <input type="text"  placeholder="Postal/Zip Code">  </td>
+				
+					
+				</tr>
+				<tr>
+				
+					<td>Email</td>
+					<td><input type="text" placeholder=" Your Email"> </td>
+				</tr>
+				<tr>
+					<td>Phone</td>
+					<td>: <input type="text" placeholder="Code"> <input type="text" placeholder="Number"> </td>
+				</tr>
+				<tr>
+					<td>Enter Password</td>
+					<td> <input type="password"placeholder=" Your Password">  </td>
+				</tr>
+				<tr>
+					<td> Confirm Password</td>
+					<td>: <input type="password" placeholder=" Confirm Password">  </td>
+				</tr>
+				
+				
+				<tr>
+					<td  rowspan="2" > <input type="reset" value="Edit">   <input type="submit" value="Update"></td>
+					
+				</tr>
+				
+				
+				</table>
+			
+			
+</center> 
+		</fieldset>
+		</form>
+	</body>
+</html>
---
+++ Mid Project/thankyou.php
@@ -0,0 +1,17 @@
+<?php 
+?> 
+<html>
+	<head></head>
+	<body>
+		<form>
+		<fieldset>
+        <center>
+        <div>
+    <h1>Thank you!</h1>
+    <p>Your submission has been received.</p>
+</div>
+        </center> 
+		</fieldset>
+		</form>
+	</body>
+</html>
